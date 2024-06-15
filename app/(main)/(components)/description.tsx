82da5c592c41729eb763f9a948b8c73c6e07fd7f
ericnd <e.ricdo@outlook.com>
2024-06-15 14:22:17 +1000

    changed typo

--- app/(main)/(components)/description.tsx
+++ app/(main)/(components)/description.tsx
@@ -4,7 +4,7 @@ export const ShortDescription = () => {
     return (
         <div className="py-8 px-12 xl:px-96">
             <p className="text-lg md:text-xl mb-4 text-center">
-                At UNSW Cat Appreciation Society, we bring together cat enthusiasts from all walks of life. Join us in celebrating our feline friends through various events, resources, and a supportive community.
+                At the UNSW Cat Appreciation Society, we bring together cat enthusiasts from all walks of life. Join us in celebrating our feline friends through various events, resources, and a supportive community.
             </p>
             <p className="text-md md:text-lg text-gray-600 text-center">
                 Whether you're a long-time cat owner or just starting to appreciate the wonders of cats, there's something for everyone here. Explore our website to learn more about upcoming events, educational resources, and ways to get involved.
